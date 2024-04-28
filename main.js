02def11dade8cda2e31d923c09c0e61563686cfa
M0hamed-ehab <mohamedehab131204@gmail.com>
2024-04-28 19:52:43 +0300

    lalalalalalalal

--- main.js
+++ main.js
@@ -329,3 +329,18 @@ function m() {
 
 }
 
+//-------------------------------------------------------Harby part start here-----------------------------------------------------------------------------------------//
+function Buy_Alert() {
+    var Buy_confirm;
+    if (confirm("are you sure you want to buy this car ?")) {
+        Buy_confirm = "You have bought this car !";
+    }
+    else {
+        Buy_confirm = "Keep looking and you may find a car that suits you better"
+    }
+    document.getElementById("buy_confirm").innerHTML = Buy_confirm;
+}
+
+function Comment_alert() {
+    alert("Thank you for your Comment, have a nice day!")
+}
